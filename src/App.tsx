eed0bb6d92a013859a81ef56c97bd64aa51380c8
Jatin Rane <jatin.rane@aauti.com>
2025-11-08 23:00:27 +0530

    fix(router): add basename to BrowserRouter for correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/stress-less-studylife/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/techniques" element={<Techniques />} />
